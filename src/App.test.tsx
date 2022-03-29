733ba1b6db4370a3b89a3ed35173c30bf07550c2
Caio Bozato Aroca <bozato@ibm.com>
2022-03-29 11:47:06 -0300

    test: button and checkbox tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,60 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+describe("button color and text change", () => {
+  test("button has correct initial color", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /change to blue/i });
+    expect(button).toHaveStyle({ backgroundColor: "red" });
+  });
+
+  test("button has correct initial text", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /change to blue/i });
+    expect(button).toHaveTextContent("Change to blue");
+  });
+
+  test("button turns blue when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /change to blue/i });
+
+    fireEvent.click(button);
+
+    expect(button).toHaveStyle({ backgroundColor: "blue" });
+  });
+
+  test("button text changes when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /change to blue/i });
+
+    fireEvent.click(button);
+
+    expect(button).toHaveTextContent(/change to red/i);
+  });
+});
+
+describe("button and checkbox", () => {
+  test("button inital condition", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: /change to blue/i });
+    expect(button).toBeEnabled();
+  });
+
+  test("checkbox inital condition", () => {
+    render(<App />);
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
+  });
+
+  test("when checkbox is clicked, the button is disabled", () => {
+    render(<App />);
+
+    const checkbox = screen.getByRole("checkbox");
+
+    fireEvent.click(checkbox);
+
+    const button = screen.getByRole("button", { name: /change to blue/i });
+
+    expect(button).toBeDisabled();
+  });
 });
